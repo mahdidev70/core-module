e674b3182b6223c27abb857ffad8f60011df47e7
next <mahdidev70@gmail.com>
2023-09-30 18:53:09 +0330

    fix: test module

--- app/Models/TestModel.php
+++ app/Models/TestModel.php
@@ -5,7 +5,7 @@ namespace Lms\Course\Http\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Test extends Model
+class Test2 extends Model
 {
     use HasFactory;
 
