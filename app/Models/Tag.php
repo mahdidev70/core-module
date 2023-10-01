3f21ed0aaf592b981d4df946747888d88cf634d9
next <mahdidev70@gmail.com>
2023-10-01 11:25:37 +0330

    feat: add tag model

---
+++ app/Models/Tag.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $guarded = [];
+    protected $table = 'core_tags';
+
+
+    public function articles()
+    {
+        return $this->morphedByMany(Article::class, 'taggable');
+    }
+}
