9a263d140d0586df7f5787693f56314b497f5a7b
next <mahdidev70@gmail.com>
2023-10-02 12:13:59 +0330

    feat: add bookmark model

---
+++ app/Models/Bookmark.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TechStudio\Core\app\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bookmark extends Model
+{
+    use HasFactory;
+    protected $table = 'core_bookmarks';
+
+
+    protected $fillable=['bookmarkable_id','bookmarkable_type','user_id'];
+    public function bookmarkable()
+    {
+        return $this->morphTo();
+    }
+}
