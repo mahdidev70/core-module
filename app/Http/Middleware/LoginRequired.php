451b4f188a4163085bd04f2f27150e1d007a65bf
next <mahdidev70@gmail.com>
2023-10-02 16:37:29 +0330

    feat: add middlware login_required

---
+++ app/Http/Middleware/LoginRequired.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace TechStudio\Core\app\Http\Middleware;
+
+use App\Helper\AccessTokenDecoder;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use App\Models\UserProfile;
+use Illuminate\Support\Facades\Auth;
+
+class LoginRequired
+{
+    public function handle(Request $request, Closure $next, $role = null)
+    {
+        if (!Config::get('flags.community')) {
+            throw new AccessDeniedHttpException('flags.community is disabled');
+        }
+        $token = $request->cookie('sa_access_token') ?? $request->bearerToken('sa_access_token');
+        if (!$token) {
+            throw new UnauthorizedHttpException('Provide a JWT token.');
+        }
+        try {
+            $decoded = AccessTokenDecoder::decode($token);
+        } catch (\DomainException | \UnexpectedValueException | \Firebase\JWT\SignatureInvalidException $e) {
+            throw new AccessDeniedHttpException($e->getMessage());
+        }
+        $user = UserProfile::where('id', $decoded->user_id)->first();
+        if (!$user) {
+            throw new AccessDeniedHttpException('The authenticated user does not exist anymore.');
+        }
+        Auth::setUser($user);
+        return $next($request);
+    }
+}
--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -11,5 +11,7 @@ class CoreServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        
+        app('router')->aliasMiddleware('login_required', TechStudio\Core\app\Http\Middleware\LoginRequired::class);
     }
 }
