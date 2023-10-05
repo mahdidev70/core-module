780379f731cac44026a0162ccbf7b9d94a708cd8
Mahdi <mahdidev70@gmail.com>
2023-10-05 15:13:29 +0330

    feat: add middlware & config

--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -9,11 +9,34 @@ class CoreServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
-        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
-        
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+
+        $this->app->make('router')->aliasMiddleware(
+            'login_required',
+            \TechStudio\Core\app\Http\Middleware\LoginRequired::class
+        );
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/flags.php',
+            'flags'
+        );
+
+        // $path = $this->publishes([
+        //     $configPathTerminy => config_path('flags.php'),
+        // ], 'config');
         // $router = $this->app['router'];
         // $router->pushMiddlewareToGroup('web', MyPackage\Middleware\WebOne::class);
-        app('router')->aliasMiddleware('login_required', \TechStudio\Core\app\Http\Middleware\LoginRequired::class);
+
+        // $this->registerConfig();
+        // app('router')->aliasMiddleware(
+        //     'login_required',
+        //     \TechStudio\Core\app\Http\Middleware\LoginRequired::class
+        // );
+    }
+
+    public function register()
+    {
+        // $this->app->register(AuthServiceProvider::class);
     }
 }
---
+++ config/flags.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'community' => true,
+    'use_dummy_email_verification_code' => true,
+    'allow_plain_text_access_token' => true,
+];
