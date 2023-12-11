4b4499184e774e1eaa5ca9c8cf9114900451f76e
behzadi <fateme.behzadi@diginext.ir>
2023-12-11 14:41:28 +0330

    add user id to user_profiles_table

---
+++ database/migrations/2023_10_11_074017_add_user_id_core_user_profiles_table.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace TechStudio\Lms\database\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('core_user_profiles', function (Blueprint $table) {
+           $table->bigInteger('user_id')->after('id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropColumns('core_user_profiles','user_id');
+    }
+};
