5463d2683aeefa532510efd4c7e0f527cabdc12a
behzadi <fateme.behzadi@diginext.ir>
2024-01-01 12:49:12 +0330

    adding local to routes

--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider
 {
