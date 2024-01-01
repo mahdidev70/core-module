bf2bd72a9463f41efb8b1b7e66224a84e55db300
behzadi <fateme.behzadi@diginext.ir>
2024-01-01 12:44:19 +0330

    adding local to routes

--- app/Providers/CoreServiceProvider.php
+++ app/Providers/CoreServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace TechStudio\Core\app\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider
@@ -11,7 +15,21 @@ class CoreServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
 
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('{locale?}/api')
+                ->group(
+                    __DIR__.'/../../routes/api.php'
+                );
+
+            // Route::middleware('web')
+            //     ->prefix('{locale?}')
+            //     ->group(base_path('routes/web.php'));
+        });
         // $this->app->make('router')->aliasMiddleware(
         //     'login_required',
         //     \TechStudio\Core\app\Http\Middleware\LoginRequired::class
