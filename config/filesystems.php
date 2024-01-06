d7febfa98a8eaf3fa7e74f72cf10c6f41a07842f
next <mahdidev70@gmail.com>
2024-01-06 11:42:47 +0330

    fix: add upload config

---
+++ config/filesystems.php
@@ -0,0 +1,123 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. The "local" disk, as well as a variety of cloud
+    | based disks are available to your application. Just store away!
+    |
+    */
+
+    'default' => env('FILESYSTEM_DISK', 'local'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been set up for each driver as an example of the required values.
+    |
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    |
+    */
+
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+            'throw' => false,
+        ],
+
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
+
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+        'blog' => [
+            'driver' => 's3',
+            'bucket' => 'blog',
+            'url' => env('AWS_URL') . '/blog',
+            'endpoint' => env('AWS_ENDPOINT'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+            // one of the following pairs will be redundant based on the version of the driver library
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'credentials' => [
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            ],
+        ],
+        'forms' => [
+            'driver' => 's3',
+            'bucket' => 'forms',
+            'url' => env('AWS_URL') . '/forms',
+            'endpoint' => env('AWS_ENDPOINT'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'use_path_style_endpoint' => true,
+            'throw' => false,
+            // one of the following pairs will be redundant based on the version of the driver library
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'credentials' => [
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            ],
+        ],
+        'community' => [
+            'driver' => 's3',
+            'bucket' => 'community-files',
+            'url' => env('AWS_URL') . '/community-files',
+            'endpoint' => env('AWS_ENDPOINT'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'use_path_style_endpoint' => true,
+            'throw' => true,
+            // one of the following pairs will be redundant based on the version of the driver library
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'credentials' => [
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            ],
+        ]
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Symbolic Links
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the symbolic links that will be created when the
+    | `storage:link` Artisan command is executed. The array keys should be
+    | the locations of the links and the values should be their targets.
+    |
+    */
+
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
+
+];
