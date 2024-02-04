f17172967c7264b3f18274160d5ced1692915b9f
Amirmahdi-for <amirmahdifor@gmail.com>
2024-02-04 15:52:40 +0330

    add faq model and controller

---
+++ app/Http/Controllers/FaqController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TechStudio\Core\app\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use TechStudio\Core\app\Http\Resources\FaqResource;
+use TechStudio\Core\app\Http\Resources\FaqsResource;
+use TechStudio\Core\app\Models\Category;
+use TechStudio\Core\app\Models\Faq;
+
+class FaqController extends Controller
+{
+    public function list($locale, Request $request) 
+    {
+        $data = Faq::where('status', 'active')->get();
+        return FaqResource::collection($data);
+    }
+
+    public function common() 
+    {
+        $faqModel = new Faq();
+        $categories = Category::where('table_type', get_class($faqModel))
+        ->select('id', 'title', 'slug', 'description', 'status', 'language')
+        ->whereHas('faq')
+        ->get();
+
+        return $categories;
+    }
+
+    public function getFaqData()
+    {
+        $data = Faq::paginate(10);
+        return new FaqsResource($data);
+    }
+
+    public function createUpdate($locale, Request $request)
+    {
+        $data = Faq::updateOrCreate(
+            ['id' => $request['id']],
+            [
+                'question' => $request['question'],
+                'answer' => $request['answer'],
+                'category_id' => $request['category'],
+            ]
+        );
+
+        return new FaqResource($data);
+    }
+
+    public function setStatus($locale, Request $request)
+    {
+        Faq::whereIn('id', $request['ids'])->update(['status' => $request['status']]);
+
+        return [
+            'updateStatus' => $request['ids']
+        ];
+    }
+}
---
+++ app/Models/Faq.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace TechStudio\Core\app\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Faq extends Model
+{
+    use HasFactory;
+
+    protected $table = 'core_faq';
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id')
+            ->where('table_type', get_class($this))->select(['id', 'slug', 'title']);
+    }
+
+}
