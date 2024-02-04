83dec9ea43aec074be7e142c9dd0291bcdfe081c
Amirmahdi-for <amirmahdifor@gmail.com>
2024-02-04 14:34:15 +0330

    fix bug for search on article

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,8 @@ namespace TechStudio\Core\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use TechStudio\Blog\app\Http\Resources\ArticleResource;
+use TechStudio\Blog\app\Models\Article;
 use TechStudio\Core\app\Models\UserProfile;
 
 class SearchController extends Controller
@@ -26,5 +28,20 @@ class SearchController extends Controller
         }
 
         return response()->json($res);
-     }
+    }
+
+    public function search(Request $request)
+    {
+        if ($request->filled('query')) {
+            $keyword = $request->get('query');
+            $result = Article::with('category', 'author')
+                ->where('title', 'like', '%' . $keyword . '%')
+                // ->orWhere('content', 'like', '%' . $keyword . '%')
+                ->take(5)->get()->map(function ($item) {
+                    $item['type'] = 'article';
+                    return $item;
+                  });;
+            return response()->json(ArticleResource::collection($result));
+        }
+    }
 }
