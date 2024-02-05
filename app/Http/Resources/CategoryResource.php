86f1d288a2de90ce008fbd847eca9ce5573e06df
Amirmahdi-for <amirmahdifor@gmail.com>
2024-02-05 19:43:41 +0330

    add avatarUrl key to category resource

--- app/Http/Resources/CategoryResource.php
+++ app/Http/Resources/CategoryResource.php
@@ -20,6 +20,7 @@ class CategoryResource extends JsonResource
             'title' => $this->title,
             'slug' => $this->slug,
             'description' => $this->description,
+            'avatarUrl' => $this->avatar_url,
             'status' => $this->status,
             'creationDate' => $this->created_at,
         ];
