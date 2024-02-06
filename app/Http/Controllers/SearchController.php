65763a1f859886d02efe378491b58bd5de512341
Amirmahdi-for <amirmahdifor@gmail.com>
2024-02-06 16:47:33 +0330

    fix return data for user search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -19,7 +19,7 @@ class SearchController extends Controller
                 $q->Where('first_name', 'like', '%'.$txt.'%')
                 ->orWhere('last_name', 'like', '%'.$txt.'%');
             })->take(10)->get(['first_name','last_name','id','avatar_url']);
-            return $users;
+
             $res = $users->map(fn($user) => [
                 'id' => $user->id,
                 'displayName' => $user->getDisplayName(),
