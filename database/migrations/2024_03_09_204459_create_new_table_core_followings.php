f4a3c04a510e5ee32f9ec5ff888f13705bee97d8
Amirmahdi-for <amirmahdifor@gmail.com>
2024-03-09 20:49:04 +0330

    add new migration for follow table

---
+++ database/migrations/2024_03_09_204459_create_new_table_core_followings.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_follow', function (Blueprint $table) {
+            $table->id();
+            $table->integer('follower');
+            $table->integer('following');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('core_followings');
+    }
+};
