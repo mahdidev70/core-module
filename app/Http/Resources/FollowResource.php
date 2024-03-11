e1c454396d18c14e070d62141463626f9f94a51b
next <mahdidev70@gmail.com>
2024-03-11 16:22:44 +0330

    fix: set is followed

--- app/Http/Resources/FollowResource.php
+++ app/Http/Resources/FollowResource.php
@@ -14,11 +14,18 @@ class FollowResource extends JsonResource
      */
     public function toArray(Request $request)
     {
+        $isFollowed = 0;
+        if (Auth('sanctum')->user()) {
+            $user = Auth('sanctum')->user();
+            $isFollowed = (boolean) Follow::where('follower_id', $user->id)->where('following_id',$this->user_id)->first();
+        }
+        
         return [
             'id' => $this->user_id,
             'displayName' => $this->getDisplayName(),
             'avatarUrl' => $this->avatar_url,
             'description' => $this->description,
+            'isFollowed' => $isFollowed,
         ];
     }
 }
