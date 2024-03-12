30c6b5c697690ea74df1a01dbd3f6fcb88129596
Amirmahdi-for <amirmahdifor@gmail.com>
2024-03-12 12:46:36 +0330

    fix bug for follow yourself

--- app/Http/Requests/StoreFollowRequest.php
+++ app/Http/Requests/StoreFollowRequest.php
@@ -26,15 +26,19 @@ class StoreFollowRequest extends FormRequest
             'followId' => [
                 'required',
                 'integer',
-                'exists:users,id', // Validate that the user being followed exists
-                // Custom rule to prevent following the same user twice
+                'exists:users,id', 
+                function ($attribute, $value, $fail) {
+                    $followerId = auth()->id();
+                    if ($followerId == $value) {
+                        $fail('شما نمیتوانید خودتان را فالو کنید');
+                    }
+                },
                 function ($attribute, $value, $fail) {
                     $followerId = auth()->id();
                     if ($this->do == 'follow' && Follow::where('follower_id', $followerId)->where('following_id', $value)->exists()) {
                         $fail('شما قبلاً این کاربر را فالو کرده‌اید.');
                     }
                 },
-                // Custom rule to ensure the user has followed the target user before unfollowing
                 function ($attribute, $value, $fail) {
                     $followerId = auth()->id();
                     if ($this->do == 'unfollow' && !Follow::where('follower_id', $followerId)->where('following_id', $value)->exists()) {
