2a8b3ed664babd6cc15107a683231562161fa239
Amirmahdi-for <amirmahdifor@gmail.com>
2024-04-02 17:32:06 +0330

    check follow user

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,10 +16,11 @@ class UserResource extends JsonResource
     {
         return [
             'id' => $this->user_id,
+            'isFollowed' => (bool) $this->where('following_id', $this->user_id)->where('follower_id', Auth('sanctum')->user()->id) ?? null,
             'displayName' => $this->getDisplayName(),
             'avatarUrl' => $this->userProfile ? $this->userProfile->avatar_url : $this->avatar_url,
-            'followersCount' => $this->follower ? $this->follower->count() : 0,
-            'followingCount' => $this->following ? $this->following->count() : 0,
+            'followersCount' => $this->follower ? $this->follower->where('following_id', $this->user_id)->count() : 0,
+            'followingCount' => $this->following ? $this->following->where('follower_id', $this->user_id)->count() : 0,
             'description' => $this->description,
         ];
     }
