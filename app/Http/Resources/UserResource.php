5dd9c38987b582471dd6e01224d854d649a29d99
Amirmahdi-for <amirmahdifor@gmail.com>
2024-04-03 14:10:29 +0330

    fix bug for selfFollow and isFollowed

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace TechStudio\Core\app\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use TechStudio\Core\app\Models\Follow;
 
 class UserResource extends JsonResource
 {
@@ -14,10 +15,23 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request)
     {
+        $isFollowed = 0;
+        $selfFollow = 0;
+        if (Auth('sanctum')->user()) {
+            $user = Auth('sanctum')->user();
+            
+            if ($user->id === $this->user_id) {
+                $selfFollow = 1;
+            }
+
+            $isFollowed = (bool) Follow::where('follower_id', $user->id)
+                ->where('following_id', $this->user_id)->first();
+        }
+
         return [
             'id' => $this->user_id,
-            'selfFollow' => (bool) (Auth('sanctum')->user()->id === $this->user_id) ?? null,
-            'isFollowed' => (bool) $this->where('following_id', $this->user_id)->where('follower_id', Auth('sanctum')->user()->id) ?? null,
+            'selfFollow' => (bool) $selfFollow,
+            'isFollowed' => (bool) $isFollowed,
             'displayName' => $this->getDisplayName(),
             'avatarUrl' => $this->userProfile ? $this->userProfile->avatar_url : $this->avatar_url,
             'followersCount' => $this->follower ? $this->follower->where('following_id', $this->user_id)->count() : 0,
