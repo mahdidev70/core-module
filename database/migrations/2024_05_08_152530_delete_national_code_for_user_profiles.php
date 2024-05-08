5f771e930cf567dad8ea55b085f8e9fc78b3c51d
Amirmahdi-for <amirmahdifor@gmail.com>
2024-05-08 15:27:44 +0330

    add new migration for delete national code in user profiles table

---
+++ database/migrations/2024_05_08_152530_delete_national_code_for_user_profiles.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('core_user_profiles', function (Blueprint $table) {
+            $table->dropColumn('national_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('core_user_profiles', function (Blueprint $table) {
+            $table->integer('national_code')->nullable()->after('status');
+        });
+    }
+};
