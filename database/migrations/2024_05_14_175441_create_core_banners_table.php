e8e300eb8f9dbbe0bdd05961bd524540e37b7db6
Amirmahdi-for <amirmahdifor@gmail.com>
2024-05-14 18:04:10 +0330

    add banners table

---
+++ database/migrations/2024_05_14_175441_create_core_banners_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('core_banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->string('link_url')->nullable();
+            $table->string('image_url');
+            $table->enum('type', ['evant', 'banner'])->nullable();
+            $table->enum('status', ['published', 'draft', 'delete'])->default('draft');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('core_banners', function (Blueprint $table) {
+            $table->dropColumn('core_banners');
+        });
+    }
+};
