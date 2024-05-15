76b34be788e35793040acbdb90460c281fbcddf4
Amirmahdi-for <amirmahdifor@gmail.com>
2024-05-15 14:52:34 +0330

    new update for banner repositories

--- app/Repositories/BannerRepository.php
+++ app/Repositories/BannerRepository.php
@@ -3,13 +3,47 @@
 namespace TechStudio\Core\app\Repositories;
 
 use Illuminate\Support\Facades\Auth;
-use TechStudio\Blog\app\Models\Banner;
+use TechStudio\Core\app\Http\Resources\BannerResource;
+use TechStudio\Core\app\Models\Banner;
 use TechStudio\Core\app\Repositories\Interfaces\BannerRepositoryInterface;
 
 class BannerRepository implements BannerRepositoryInterface
 {
+    public function list() 
+    {
+        $data = Banner::orderBy('id', 'DESC')->paginate(10);
+        return $data;
+    }
+
+    public function createUpdate($request)
+    {
+        $banner = Banner::updateOrCreate(
+            ['id' => $request['id']],
+            [
+                'title' => $request['title'],
+                'description' => $request['description'],
+                'link_url' => $request['linkUrl'],
+                'image_url' => $request['imageUrl'],
+                'type' => $request['type'],
+                'status' => 'draft',
+            ]
+        );
+        return $banner;
+    }
+
+    public function setStatus($request) 
+    {
+        
+    }
+
     public function getBannerForHomPage()
     {
         return Banner::select('title','link_url as linkUrl','image_url as imageUrl')->get();
+    }
+
+    public function event() 
+    {
+        $data = Banner::where('type', 'evant')->orderBy('id', 'DESC')->get();
+        return BannerResource::collection($data);
     }
 }
--- app/Repositories/Interfaces/BannerRepositoryInterface.php
+++ app/Repositories/Interfaces/BannerRepositoryInterface.php
@@ -5,4 +5,8 @@ namespace TechStudio\Core\app\Repositories\Interfaces;
 interface BannerRepositoryInterface
 {
     public function getBannerForHomPage();
+    public function list();
+    public function createUpdate($request);
+    public function setStatus($request);
+    public function event();
 }
