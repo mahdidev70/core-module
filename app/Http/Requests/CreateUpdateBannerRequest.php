f79644c45dd74762546070af3cb2f1c786f8ba4b
Amirmahdi-for <amirmahdifor@gmail.com>
2024-05-15 14:52:08 +0330

    add validation for createUpdate banner

---
+++ app/Http/Requests/CreateUpdateBannerRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace TechStudio\Core\app\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateUpdateBannerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string'],
+            'description' => ['nullable', 'string'],
+            'linkUrl' => ['nullable', 'string'],
+            'imageUrl' => ['required', 'string'],
+            'type' => ['nullable', 'in:event, banner'],
+            'status' => ['nullable', 'in:published,draft,delete'],
+        ];
+    }
+}
