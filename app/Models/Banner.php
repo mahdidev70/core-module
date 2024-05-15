7b48d982e1af25090fe4360e2dfa770e3e611c0e
Amirmahdi-for <amirmahdifor@gmail.com>
2024-05-15 14:51:40 +0330

    add globalScope for banner in model

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -2,16 +2,27 @@
 
 namespace TechStudio\Core\app\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Banner extends Model
 {
-    use HasFactory , SoftDeletes;
+    use HasFactory;
 
     protected $table = 'core_banners';
 
     protected $guarded = ['id'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        if (!request()->is(['*/api/panel/*',])) {
+            static::addGlobalScope('published', function (Builder $builder) {
+                $builder->where('status', 'published');
+            });
+        }
+    }
 
 }
